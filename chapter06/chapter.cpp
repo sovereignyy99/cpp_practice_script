1bba06e5885584411fa7c9ac3586a5bfa44a87f6
szy99 <sovereignyy@gmail.com>
2018-03-29 11:50:04 +0800

    chapter06 ex3-5 done.

--- chapter06/chapter.cpp
+++ chapter06/chapter.cpp
@@ -28,13 +28,104 @@ using std::runtime_error;
 using std::string;
 using std::vector;
 
+
 #if 1
-// 6
+// 6.6
+unsigned int AbsValue(int);
+
 int main(int argc, char **agrv)
 {
 
-	cout << "hello,world.";
 
 	return 0;
+}
+
+unsigned int AbsValue(int i)
+{
+	return i >= 0 ? i : -i;
+}
+#endif
+
+#if 0
+// 6.5
+unsigned int AbsValue(int);
+
+int main(int argc, char **agrv)
+{
+	// 编写一个求阶乘的函数
+	cout << "Enter number: \n";
+	int i;
+	cin >> i;
+	unsigned int j=	AbsValue(i);
+	cout << "|" << i << "| = " << j << endl;
+
+	return 0;
+}
+
+unsigned int AbsValue(int i)
+{
+	return i >= 0 ? i : -i;
+}
+#endif
+
+#if 0
+// 6.4
+// function fundamental
+void factorial_with_interacts();
+
+int main(int argc, char **agrv)
+{
+	// 编写一个求阶乘的函数
+	factorial_with_interacts();
+
+	return 0;
+}
+
+void factorial_with_interacts()
+{
+	int num;
+	std::cout << "Please input a positive number: ";
+	while (std::cin >> num && num < 0)
+		std::cout << "Please input a positive number again: ";
+	std::cout << num;
+
+	unsigned long long result = 1;
+	while (num > 1)
+		result *= num--;
+
+	std::cout << "! is ";
+	if (result)
+		std::cout << result << std::endl;
+	else
+		std::cout << "too big" << std::endl;
+}
+#endif
+
+#if 0
+// 6.3
+// function fundamental
+long long Factorial(short int f_iNumber);
+
+int main(int argc, char **agrv)
+{
+	// 编写一个求阶乘的函数
+	cout << "Enter number:\n";
+	short i;
+	cin >> i;
+	long long lNum = Factorial(i);
+	cout << i << " : " << lNum << endl;
+
+	return 0;
+}
+
+long long Factorial(short int f_iNumber)
+{
+	long i = 1;
+	while (f_iNumber >= 1)
+	{
+		i *= f_iNumber--;
+	}
+
+	return i;
 }
 #endif
