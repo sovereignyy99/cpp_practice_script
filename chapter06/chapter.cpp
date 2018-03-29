7feca17b0ba4fc5385209277ab4aa232929e02ec
szy99 <sovereignyy@gmail.com>
2018-03-29 11:22:41 +0800

    add chapter06 files

---
+++ chapter06/chapter.cpp
@@ -0,0 +1,40 @@
+/*
+ * @Author: MattewPerry
+ * @Date: 2018-03-15 15:11:54
+ * @Last Modified by:   MattewPerry
+ * @Last Modified time: 2018-03-15 15:11:54
+ */
+
+#include <cctype>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <iterator>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+// using namespace std;
+using std::begin;
+using std::cin;
+using std::cout;
+using std::end;
+using std::endl;
+using std::iterator;
+using std::pair;
+using std::runtime_error;
+using std::string;
+using std::vector;
+
+#if 1
+// 6
+int main(int argc, char **agrv)
+{
+
+	cout << "hello,world.";
+
+	return 0;
+}
+#endif
